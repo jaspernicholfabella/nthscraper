9fa6045b5d272ebd811cfa498ae9b6663571d370
Jasper Nichol M Fabella <jaspernicholfabella@gmail.com>
2024-05-02 15:01:00 +0800

    created scraping get response logic & tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import requests
 from zenscraper.wrapper.local_file_adapter import LocalFileAdapter
+from zenscraper.scraper import ZenScraper
 
 
 class TestLocalFileAdapter(unittest.TestCase):
@@ -9,7 +10,7 @@ class TestLocalFileAdapter(unittest.TestCase):
 
     def test_local_file_adapter_get(self):
         """test to check if file local adapter works properly.
-           testing if we can use request library on our local files.
+        testing if we can use request library on our local files.
         """
         file_path = os.path.abspath(os.path.curdir) + "/tests/httpbin.html"
         print("FILE PATH:", file_path)
@@ -17,6 +18,24 @@ class TestLocalFileAdapter(unittest.TestCase):
         session.mount("file://", LocalFileAdapter())
         res = session.get(f"file://{file_path}")
         self.assertEqual(res.status_code, 200)
+
+
+class TestZenScraper(unittest.TestCase):
+    """tests to check scraping on a website"""
+
+    def setUp(self):
+        self.scraper = ZenScraper()
+        self.local_file_path = os.path.abspath(os.path.curdir)
+
+    def test_scraping_online_site(self):
+        scraper = self.scraper.get("https://httpbin.org", sleep_seconds=1)
+        self.assertEqual(scraper.status_code, 200)
+
+    def test_scraping_offline_site(self):
+        scraper = self.scraper.get_from_local(
+            f"{self.local_file_path}/tests/httpbin.html"
+        )
+        self.assertEqual(scraper.status_code, 200)
 
 
 if __name__ == "__main__":
--- zenscraper/scraper.py
+++ zenscraper/scraper.py
@@ -1,1 +1,52 @@
+import os
+from urllib.request import url2pathname
+import random
+import requests
+from requests.models import Response
+from lxml import html
+from lxml.etree import _Element, tostring
+from typing import Tuple, Optional, List, Any
+from zenscraper.wrapper.requests_wrapper import RequestsWrapper
+from zenscraper.wrapper.local_file_adapter import LocalFileAdapter
 
+
+class ZenScraper:
+    """class to scrape websites following selenium-like rules"""
+
+    def __init__(self) -> None:
+        self.requests_wrapper: RequestsWrapper = RequestsWrapper()
+        self.doc = None
+        self.response: Optional[Response] = None
+
+    def _get_response(
+        self,
+        url: str,
+        sleep_seconds: Optional[int] = None,
+        is_post: bool = False,
+        **kwargs: Any,
+    ):
+        sleep_seconds = (
+            sleep_seconds if sleep_seconds is not None else random.randint(1, 5)
+        )
+        method = self.requests_wrapper.post if is_post else self.requests_wrapper.get
+        self.response = method(url, sleep_seconds=sleep_seconds, **kwargs)
+        self.doc = (
+            html.fromstring(self.response.content) if self.response.content else None
+        )
+        return self.response
+
+    def get(self, url: str, sleep_seconds: Optional[int] = None, **kwargs: Any):
+        return self._get_response(url, sleep_seconds, **kwargs)
+
+    def get_from_local(self, file_path: str) -> Response:
+        session = requests.session()
+        session.mount("file://", LocalFileAdapter())
+        self.response = session.get(f"file://{file_path}")
+        self.doc = (
+            html.fromstring(self.response.content) if self.response.content else None
+        )
+        self.response.close()
+        return self.response
+
+    def post(self, url: str, sleep_seconds: Optional[int] = None, **kwargs: Any):
+        return self._get_response(url, sleep_seconds, is_post=True, **kwargs)
